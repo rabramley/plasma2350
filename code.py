5363d4a829ad90f847a0d680334c17514f4b7b6b
Richard Bramley <rabramley@gmail.com>
2024-12-08 08:10:24 +0000

    Refactoring

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
+# type: ignore
 
 """CircuitPython Essentials NeoPixel example"""
 import time
@@ -31,16 +32,27 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
+class pallet:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    YELLOW = (255, 150, 0)
+    GREEN = (0, 255, 0)
+    CYAN = (0, 255, 255)
+    BLUE = (0, 0, 255)
+    PURPLE = (180, 0, 255)
 
-print("Yellow")
+    def colours(self):
+        return [
+            self.RED,
+            self.YELLOW,
+            self.GREEN,
+            self.CYAN,
+            self.BLUE,
+            self.PURPLE,
+        ]
 
-while True:
+def flash_colours(pixels, RED, GREEN, BLUE):
     pixels.fill(RED)
     pixels.show()
     time.sleep(1)
@@ -51,6 +63,7 @@ while True:
     pixels.show()
     time.sleep(1)
 
+def chase_colours(color_chase, RED, YELLOW, GREEN, CYAN, BLUE, PURPLE):
     color_chase(RED, 0.1)  # Increase the number to slow down the color chase
     color_chase(YELLOW, 0.1)
     color_chase(GREEN, 0.1)
@@ -58,5 +71,10 @@ while True:
     color_chase(BLUE, 0.1)
     color_chase(PURPLE, 0.1)
 
+while True:
+    flash_colours(pixels, pallet.RED, pallet.GREEN, pallet.BLUE)
+
+    chase_colours(color_chase, pallet.RED, pallet.YELLOW, pallet.GREEN, pallet.CYAN, pallet.BLUE, pallet.PURPLE)
+
     rainbow_cycle(0)  # Increase the number to slow down the rainbow
- # type: ignore
+ 
